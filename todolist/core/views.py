f8b23f47d80b36babcb7b73ee811b9ad727b37f3
Stavitskii <pochta100@mail.ru>
2022-12-05 15:18:14 +0300

    Update password does not work

--- todolist/core/views.py
+++ todolist/core/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model, login, logout
 from rest_framework import generics, status, permissions
 from rest_framework.response import Response
 
-from .serializers import RegistrationSerializer, LoginSerializer, ProfileSerializer
+from .serializers import RegistrationSerializer, LoginSerializer, ProfileSerializer, UpdatePasswordSerializer
 
 USER_MODEL = get_user_model()
 
@@ -36,3 +36,10 @@ class ProfileView(generics.RetrieveUpdateDestroyAPIView):
         logout(request)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+
+class UpdatePasswordView(generics.UpdateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UpdatePasswordSerializer
+
+    def get_object(self):
+        return self.request.user
