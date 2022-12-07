da16689486dc6a651ec23d077720d74d0e3c0270
Stavitskii <pochta100@mail.ru>
2022-12-07 08:04:01 +0300

    00:21:27

--- todolist/goals/models.py
+++ todolist/goals/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.utils import timezone
 
-from core.models import User
-
 
 class GoalCategory(models.Model):
     class Meta:
@@ -10,7 +8,7 @@ class GoalCategory(models.Model):
         verbose_name_plural = "Категории"
 
     title = models.CharField(verbose_name="Название", max_length=255)
-    user = models.ForeignKey(User, verbose_name="Автор", on_delete=models.PROTECT)
+    user = models.ForeignKey('core.User', verbose_name="Автор", on_delete=models.PROTECT)
     is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
     created = models.DateTimeField(verbose_name="Дата создания")
     updated = models.DateTimeField(verbose_name="Дата последнего обновления")
@@ -21,3 +19,25 @@ class GoalCategory(models.Model):
         self.updated = timezone.now()  # проставляем дату обновления
         return super().save(*args, **kwargs)
 
+
+class Goal(models.Model):
+    class Status(models.IntegerChoices):
+        to_do = 1, "К выполнению"
+        in_progress = 2, "В процессе"
+        done = 3, "Выполнено"
+        archived = 4, "Архив"
+
+    class Priority(models.IntegerChoices):
+        low = 1, "Низкий"
+        medium = 2, "Средний"
+        high = 3, "Высокий"
+        critical = 4, "Критический"
+
+    created = models.DateTimeField(verbose_name="Дата создания")
+    updated = models.DateTimeField(verbose_name="Дата последнего обновления")
+
+    def save(self, *args, **kwargs):
+        if not self.id:  # Когда объект только создается, у него еще нет id
+            self.created = timezone.now()  # проставляем дату создания
+        self.updated = timezone.now()  # проставляем дату обновления
+        return super().save(*args, **kwargs)
